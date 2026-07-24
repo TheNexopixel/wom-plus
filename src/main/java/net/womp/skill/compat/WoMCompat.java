4b3fec53ade14b28c4e0a4519f9cc1cb6b304a7c
Sidtheplayer <140822160+Sidtheplayer@users.noreply.github.com>
2026-07-24 23:01:44 +0530

    Refactor advancedGuardMotions for shield category

--- src/main/java/net/womp/skill/compat/WoMCompat.java
+++ src/main/java/net/womp/skill/compat/WoMCompat.java
@@ -97,11 +97,11 @@ public class WoMCompat implements ICompatModule {
                 Animations.BIPED_HIT_SHIELD);
         guardBreakMotions.put(CapabilityItem.WeaponCategories.SHIELD, (item, player) ->
                 Animations.BIPED_COMMON_NEUTRALIZED);
-        advancedGuardMotions.put(CapabilityItem.WeaponCategories.SHIELD, (itemCap, playerpatch) ->
-                if(itemCap.getInnateSkill(playerpatch,playerpatch.getAdvancedHoldingItemStack(InteractionHand.MAIN_HAND)).equals(WOMPSkills.RAAAHHH)){
+        advancedGuardMotions.put(CapabilityItem.WeaponCategories.SHIELD, (itemCap, playerpatch) -> {
+                if(playerpatch.getHoldingItemCapability(InteractionHand.MAIN_HAND).getInnateSkill(playerpatch,playerpatch.getAdvancedHoldingItemStack(InteractionHand.MAIN_HAND)).equals(WOMPSkills.RAAAHHH)){
             return WOMPAnimations.HOLLOW_GUARD_STANCE_COUNTER;
         }else return playerpatch.getHoldingItemCapability(InteractionHand.OFF_HAND) instanceof GesetzCapability ? AnimsHerrscher.HERRSCHER_TRANE : Animations.SWEEPING_EDGE;
-                    
+    }
                             );
 
 
